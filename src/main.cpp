2bf39e6e1b20c6fe9d8b8aa7b577b408af1481ee
Adrian Schurz <ulmeyda.smile@gmail.com>
2020-05-16 21:42:54 +0200

    Start wifi and sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,43 @@
 #include <Arduino.h>
+#include <Secrets.hpp>
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include "SparkFun_SCD30_Arduino_Library.h"
+  
+SCD30 airSensor;
+
+void startSensor(){
+  bool sensorReady = airSensor.begin();
+  if (!sensorReady)
+  {
+    Serial.println("Sensor wiring/setup faulty. Not continuing...");
+    while (1)
+      ;
+  }
+
+}
+
+void setupI2C(){
+  Wire.begin();
+}
+
+void waitForWifiConnection(){
+  Serial.print("Connecting to Wifi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void startWifiConnection(){
+  WiFi.begin(SSID, PWD);
+  waitForWifiConnection();
+}
+
 void waitForSerialConnection() {
   while (!Serial) {;}
 }
@@ -10,6 +49,10 @@ void startSerialConnection() {
 
 void setup() {
   startSerialConnection();
+  startWifiConnection();
+  setupI2C();
+
+  startSensor();
 }
 
 void loop() {
