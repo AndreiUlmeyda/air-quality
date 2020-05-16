fbac7bfd421ebdc87d8a2a2f014c05e936551e6b
Adrian Schurz <ulmeyda.smile@gmail.com>
2020-05-16 21:33:50 +0200

    Start serial connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <Arduino.h>
+void waitForSerialConnection() {
+  while (!Serial) {;}
+}
+
+void startSerialConnection() {
+  Serial.begin(9600);
+  waitForSerialConnection();
+}
 
 void setup() {
+  startSerialConnection();
 }
 
 void loop() {
