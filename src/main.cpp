17c4719199ac3ca400690f3e37519b0f719c89a8
Adrian Schurz <ulmeyda.smile@gmail.com>
2020-05-17 18:25:28 +0200

    Gather data, send it to influxDB

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,46 @@
 #include <Arduino.h>
+#include <Config.hpp>
 #include <Secrets.hpp>
 #include <ESP8266WiFi.h>
+#include <ArduinoHttpClient.h>
 #include <Wire.h>
 #include "SparkFun_SCD30_Arduino_Library.h"
   
 SCD30 airSensor;
+
+void buildAndSendHttpRequestWithMeasurement(HttpClient *httpClient, int co2, int temperature, int humidity) {
+  String path = "/write?db=" + String(databaseName);
+  String contentType = "application/json";
+  String fieldSeparator = String(",");
+  String content = "air-quality co2=" + String(co2) + fieldSeparator;
+  content = content + "temperature=" + String(temperature) + fieldSeparator;
+  content = content + "humidity=" + String(humidity);
+
+  httpClient->beginRequest();
+
+  httpClient->post(path);
+  httpClient->sendHeader("Content-Type", contentType);
+  httpClient->sendHeader("Content-Length", content.length());
+  httpClient->beginBody();
+  httpClient->print(content);
+
+  httpClient->endRequest();
+}
+
+void printResponse(HttpClient *httpClient) {
+   int statusCode = httpClient->responseStatusCode();
+  String responseBody = httpClient->responseBody();
+
+  Serial.println(statusCode);
+  Serial.println(responseBody);
+}
+
+void sendSensorDataToServer(int co2, int temperature, int humidity){
+  WiFiClient wifi;
+  HttpClient httpClient = HttpClient(wifi, databaseHost, databasePort);
+  buildAndSendHttpRequestWithMeasurement(&httpClient, co2, temperature, humidity);
+  printResponse(&httpClient);
+}
 
 void calibrateSensor(){
   // measurement interval in seconds
@@ -60,10 +96,16 @@ void setup() {
   startSerialConnection();
   startWifiConnection();
   setupI2C();
-
   startSensor();
   calibrateSensor();
 }
 
 void loop() {
+  if (airSensor.dataAvailable())
+  {
+    int co2 = airSensor.getCO2();
+    int temperature = airSensor.getTemperature();
+    int humidity = airSensor.getHumidity();
+    sendSensorDataToServer(co2, temperature, humidity);
+  }
 }
