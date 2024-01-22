24639c654bb0810b3f1bdfa01842d6ca904f388a
Adrian Schurz <adrian.schurz@check24.de>
2024-01-22 15:10:32 +0100

    Achieve display control

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "SparkFun_SCD30_Arduino_Library.h"
+#include <Adafruit_SSD1331.h>
 
 SCD30 airSensor;
+
+// https://digilent.com/reference/pmod/pmodoledrgb/start
+#define CS 4    // chip select
+#define DC 21   // data/command control
+#define MOSI 18 // master-out slave-in
+#define SCK 5   // serial clock
+#define RES 14  // power reset
+
+Adafruit_SSD1331 display = Adafruit_SSD1331(CS, DC, MOSI, SCK, RES);
+
+#define black 0x0000
+#define blue 0x001F
+#define red 0xF800
+#define green 0x07E0
+#define cyan 0x07FF
+#define magenta 0xF81F
+#define yellow 0xFFE0
+#define white 0xFFFF
 
 void calibrateSensor()
 {
@@ -49,6 +68,12 @@ void startSerialConnection()
   waitForSerialConnection();
 }
 
+void startDisplay()
+{
+  display.begin();
+  display.fillScreen(green);
+}
+
 // entry point
 void setup()
 {
@@ -56,11 +81,11 @@ void setup()
   setupI2C();
   startSensor();
   calibrateSensor();
+  startDisplay();
 }
 
 void loop()
 {
-  delay(1000);
   if (airSensor.dataAvailable())
   {
     int co2 = airSensor.getCO2();
